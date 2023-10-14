857379f697cc14ce6e4b7ddb2272b6b41c06ffe7
rok0705 <leeseon0503@gmail.com>
2023-10-14 15:26:20 -0400

    project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://accounts.google.com/signup")
+time.sleep(10)
