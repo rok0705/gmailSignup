a5048681f3a04c5e0133d7cbb9ec7c67890b0e8b
rok0705 <leeseon0503@gmail.com>
2023-10-15 15:22:13 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pytest
 
 if __name__ == "__main__":
+    # execute All test cases.
     retcode = pytest.main()
+
+    # execute performance related test cases only.
+    retcode = pytest.main(["-k","test_performance.py"])
+
+    # execute functional test cases only.
+    retcode = pytest.main(["-k", "test_functional.py"])
