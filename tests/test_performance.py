58707749fc5af2c16fee6e531994449b47fa3c3d
rok0705 <leeseon0503@gmail.com>
2023-10-19 01:01:58 -0400

    update stress test case

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -20,8 +20,10 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 def is_gmail_backend_down():
   driver.implicitly_wait(10)
   driver.get("https://accounts.google.com/signup")
+  print("Response time Test: PASS")
   return 0
-def multiple_clients_openURL():
+def multiple_clients_openURL(scale):
+  number_of_clients = scale
   def gmailClient(number, result):
     driver = webdriver.Chrome()
     driver.get("https://accounts.google.com/signup")
@@ -31,18 +33,16 @@ def multiple_clients_openURL():
       result[number] = "fail"
       return
     # tabs.
-    for i in range(1,10):
+    for i in range(1,number_of_clients):
       driver.execute_script("window.open('https://accounts.google.com/signup');")
       driver.switch_to.window(driver.window_handles[i])
       title = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[1]/div/h1/span").text
       if title != "Create a Google Account":
         result[number] = "fail"
         return
-      # print("checkup ",i," done.")
 
-  number_of_clients = 10
   threads = []
-  results = [""] * 10
+  results = [""] * number_of_clients
 
   for number in range(number_of_clients):
     t = Thread(target=gmailClient, args=(number, results))
@@ -54,10 +54,15 @@ def multiple_clients_openURL():
 
   if "fail" in "".join(results):
     return -1
+  print("Stress Test: " + str(number_of_clients) + " concurrent clients and tabs : PASS")
   return 0
 
 # PERFORMANCE tests
 def test_is_gmail_backend_down():
   assert is_gmail_backend_down() == 0
 def test_multiple_clients():
-  assert multiple_clients_openURL() == 0
+  scale = 2
+  loops = 4
+  # until failure is hit.
+  for i in range(1, loops):
+    assert multiple_clients_openURL(i*scale) == 0
